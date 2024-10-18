7480fa26b7b4e6c6771b632b8a10ba34cd31e8cb
hfccr <hfccr@outlook.com>
2024-10-18 23:07:31 +0530

    Add utility functions

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -14,19 +14,65 @@ contract Factory {
     }
     address[] public deployedContracts;
     uint256 public count = 0;
+    struct Account {
+        string profile;
+        Provider provider;
+    }
+    mapping(uint256 => address) public deployedContract;
+    mapping(address => Account) public deployedAccounts;
+
+    // Create a mapping from Provider to profile to bool to check if a contract for this was deployed
+    mapping(Provider => mapping(string => bool)) public deployedProfiles;
+
+    struct DeployedContract {
+        address contractAddress;
+        string profile;
+        Provider provider;
+        uint256 id;
+    }
 
     function createContract(Provider provider, string memory profile) public {
+        // Check if a contract for this profile was already deployed
+        require(
+            !deployedProfiles[provider][profile],
+            "Contract for this profile already deployed"
+        );
         string memory ticker = string(
-            abi.encodePacked("BS", Strings.toString(count))
+            abi.encodePacked("2B", Strings.toString(count))
         );
         address tokens = address(
             new Token("2Based", ticker, provider, profile)
         );
+        deployedAccounts[tokens] = Account(profile, provider);
         deployedContracts.push(tokens);
         count++;
     }
 
-    function getDeployedContracts() public view returns (address[] memory) {
-        return deployedContracts;
+    function getDeployedContracts()
+        public
+        view
+        returns (DeployedContract[] memory)
+    {
+        DeployedContract[] memory contracts = new DeployedContract[](
+            deployedContracts.length
+        );
+        for (uint256 i = 0; i < deployedContracts.length; i++) {
+            address contractAddress = deployedContracts[i];
+            Account memory account = deployedAccounts[contractAddress];
+            contracts[i] = DeployedContract(
+                contractAddress,
+                account.profile,
+                account.provider,
+                i
+            );
+        }
+        return contracts;
+    }
+
+    function isCreated(
+        Provider provider,
+        string memory profile
+    ) public view returns (bool) {
+        return deployedProfiles[provider][profile];
     }
 }
