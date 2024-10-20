ab8ec3cc29600f8ac532a33a226aff84f30f2ee6
hfccr <hfccr@outlook.com>
2024-10-20 16:20:11 +0530

    Add to balance

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -37,6 +37,7 @@ contract Token is ERC20, Ownable {
     uint256 public tokenId;
     address public tokenOwner;
     bool public claimed;
+    mapping(address => uint256) public userBalance;
 
     constructor(
         address _factoryAddress,
@@ -89,7 +90,8 @@ contract Token is ERC20, Ownable {
 
         totalSupplyTokens -= _amount;
         _burn(msg.sender, _amount); // Burn the sold tokens
-        payable(msg.sender).transfer(cost.cost); // Send revenue after deducting fee
+        userBalance[msg.sender] += cost.cost;
+        // payable(msg.sender).transfer(cost.cost); // Send revenue after deducting fee
         feeBalance += cost.fee; // Add fee to the fee balance
 
         // Transfer the fee to the owner's address
